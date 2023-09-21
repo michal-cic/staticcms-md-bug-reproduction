4e015be3b71aba731dbbec19aae53bfa52816b50
michal-cic <michal.cic@gmail.com>
2023-09-21 09:03:40 +0200

    replaced a with Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>staticcms-md-bug-reproduction</h1>
       <div>
-        <a href="/admin" className="underline">
+        <Link href="/admin" className="underline">
           Admin page
-        </a>
+        </Link>
       </div>
     </div>
   );
